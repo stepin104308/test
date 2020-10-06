da29608014775d4ae693b28819afca64e3a8c063
Visvak Kumar <visvakumar.s99@gmail.com>
2020-10-07 01:37:30 +0530

    First commit

---
+++ inc/function.h
@@ -0,0 +1,11 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <stdio.h>
+#include<conio.h>
+
+void fibonacci_func(int fibonacci_limit);
+int factorial_func(int factorial_input);
+int prime_func(int prime_input);
+
+#endif
